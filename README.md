22246b896286aad9c866de434cb1491b6cd91923
Campbell <campbellrehu@me.com>
2023-06-14 21:05:24 +1200

    Update file structure and script to accept args

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 Repository for testing out creating symbolic links
 
-Running the `run.sh` script will create symbolic links in the `dest-folder` to the files in the `./source-folder/`
+# Using the `./run.sh` script
+
+Use the following command:
+
+```bash
+./run.sh source_dir target_dir
+```
+
+This will replicate the directory structure from `source_dir` in `target_dir` and create symbolic links between all files in the directories.
--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-source_dir="$PWD/source-folder"
-target_dir="$PWD/dest-folder"
+src="$1"
+target="$2"
 
-create_symlinks() {
-    local src="$1"
-    local target="$2"
-
+if [ "$#" -eq 2 ]; then
     find "$src" -mindepth 1 -type d | awk -v src="$src" '{gsub(src"/?", "", $0); print}' | while read -r dir; do
         # Create directory structure in the target directory
         mkdir -p "$target/$dir"
@@ -16,7 +13,6 @@ create_symlinks() {
         # Create symbolic links in the corresponding directory
         ln -s "$src/$file" "$target/$file"
     done
-}
-
-# Call the function with source and target directories
-create_symlinks "$source_dir" "$target_dir"
+else
+    echo "You must pass a source_dir and target_dir to this script"
+fi
