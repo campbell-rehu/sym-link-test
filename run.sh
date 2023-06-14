778175cede4c00fb41939cef2392940536a9f0bd
Campbell <campbellrehu@me.com>
2023-06-15 06:29:31 +1200

    Add dryRun mode

--- run.sh
+++ run.sh
@@ -2,16 +2,25 @@
 
 src="$1"
 target="$2"
+dryRun="$3"
 
-if [ "$#" -eq 2 ]; then
+if [ "$#" -ge 2 ]; then
     find "$src" -mindepth 1 -type d | awk -v src="$src" '{gsub(src"/?", "", $0); print}' | while read -r dir; do
-        # Create directory structure in the target directory
-        mkdir -p "$target/$dir"
+        if [ -n "$dryRun" ]; then
+            echo "DryRun: mkdir -p \"$target/$dir\""
+        else
+            # Create directory structure in the target directory
+            mkdir -p "$target/$dir"
+        fi
     done
 
     find "$src" -type f | awk -v src="$src" '{gsub(src"/?", "", $0); print}' | while read -r file; do
-        # Create symbolic links in the corresponding directory
-        ln -s "$src/$file" "$target/$file"
+        if [ -n "$dryRun" ]; then
+            echo "DryRun: ln -s \"$src/$file\" \"$target/$file\""
+        else
+            # Create symbolic links in the corresponding directory
+            ln -s "$src/$file" "$target/$file"
+        fi
     done
 else
     echo "You must pass a source_dir and target_dir to this script"
