9937954310b8a51a42c04aafac503458006c0710
Campbell <campbellrehu@me.com>
2023-06-17 19:12:06 +1200

    Update script to copy certain files instead of symlink

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Repository for testing out creating symbolic links
 Use the following command:
 
 ```bash
-./run.sh source_dir target_dir
+./run.sh [--dry-run] source_dir target_dir files_to_copy_instead_of_symlink
 ```
 
 This will replicate the directory structure from `source_dir` in `target_dir` and create symbolic links between all files in the directories.
--- run.sh
+++ run.sh
@@ -1,38 +1,73 @@
 #!/bin/bash
 
-src="$1"
-target="$2"
-dryRun="$3"
+# Function to create symbolic links or copy specific files while preserving the directory structure
+process_files() {
+    local src="${1%/}"  # Remove trailing slash if present
+    local target="${2%/}"  # Remove trailing slash if present
+    shift 2
+    local files_to_copy=("$@")
+    local exclude=()
 
-exclude_dirs=(".git")
-
-if [ "$#" -ge 2 ]; then
-    exclude=()
+    # Hard-coded directories to exclude
+    local exclude_dirs=("dir_to_exclude1" "dir_to_exclude2")
 
     # Build the exclude options for find command
     for dir in "${exclude_dirs[@]}"; do
         exclude+=(-not -path "$src/$dir/*" -not -path "$src/$dir")
     done
 
-    echo "Exclude options: ${exclude[@]}"
+    # Process files
+    find "$src" -type f "${exclude[@]}" | while read -r file; do
+        # Get the relative path of the file with respect to the source directory
+        rel_path=${file#$src/}
 
-    find "$src" -mindepth 1 -type d "${exclude[@]}" | awk -v src="$src" '{gsub(src"/?", "", $0); print}' | while read -r dir; do
-        if [ -n "$dryRun" ]; then
-            echo "DryRun: mkdir -p \"$target/$dir\""
+        # Debug: Print paths being processed
+        # echo "Processing file: $file"
+        # echo "Relative path: $rel_path"
+
+        # Check if the file is in the list of files to copy
+        if printf '%s\n' "${files_to_copy[@]}" | grep -Fxq -- "$file"; then
+            # Only copy if the file does not already exist
+            if ! [ -e "$target/$rel_path" ]; then
+                # Copy the file
+                if $dry_run; then
+                    echo "Would copy: $file to $target/$rel_path"
+                else
+                    mkdir -p "$(dirname "$target/$rel_path")"
+                    cp "$file" "$target/$rel_path"
+                fi
+            fi
         else
-            # Create directory structure in the target directory
-            mkdir -p "$target/$dir"
+            # Create a symlink
+            if $dry_run; then
+                echo "Would create symlink: $file to $target/$rel_path"
+            else
+                mkdir -p "$(dirname "$target/$rel_path")"
+                ln -sf "$file" "$target/$rel_path"
+            fi
         fi
     done
+}
 
-    find "$src" -type f "${exclude[@]}" | awk -v src="$src" '{gsub(src"/?", "", $0); print}' | while read -r file; do
-        if [ -n "$dryRun" ]; then
-            echo "DryRun: ln -sf \"$src/$file\" \"$target/$file\""
-        else
-            # Create symbolic links in the corresponding directory
-            ln -sf "$src/$file" "$target/$file"
-        fi
-    done
-else
-    echo "You must pass a source_dir and target_dir to this script"
+# Check if the script is in dry-run mode
+dry_run=false
+if [[ $1 == "--dry-run" ]]; then
+    dry_run=true
+    shift 1
 fi
+
+# Check if at least two arguments are provided (source and target directories)
+if [ "$#" -lt 2 ]; then
+    echo "Usage: ./script.sh [--dry-run] source_dir target_dir full_path_of_files_to_copy..."
+    exit 1
+fi
+
+# Get the source and target directories from command line arguments
+source_dir="$1"
+target_dir="$2"
+
+# Shift the first two arguments (source and target directories)
+shift 2
+
+# Call the function with source and target directories and list of files to copy
+process_files "$source_dir" "$target_dir" "$@"
